53debbd382ef592c85c99b7efaa84a68ba69c62c
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-03 14:36:23 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,16 +20,13 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="post.php">Post</a>
+          <a class="nav-link" href="fighter.php">fighter</a>
         </li>
           <li class="nav-item">
-          <a class="nav-link" href="post-result.php">Post result</a>
+          <a class="nav-link" href="gym.php">gym</a>
         </li>
           <li class="nav-item">
-          <a class="nav-link" href="get.php">Get</a>
-        </li>
-          <li class="nav-item">
-          <a class="nav-link" href="get-result.php">Get result</a>
+          <a class="nav-link" href="fighters-with-gyms.php">fighters with gyms</a>
         </li>
       </ul>
     </div>
