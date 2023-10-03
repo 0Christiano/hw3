14e6bfcf19022be51c611985d4cd3d1d0c06a0df
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-03 16:11:11 -0500

    Update model-fighter.php

--- model-fighter.php
+++ model-fighter.php
@@ -1,5 +1,5 @@
 <?php
-function selectfighter() {
+function selectfighters() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT fighter_ID,fighter_name,weight FROM `fighter` ");
