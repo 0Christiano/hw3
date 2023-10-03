e973660d7bebb9e777411dbb6ba914e60445c044
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-03 16:37:12 -0500

    Create gyms-by-fighter.php

---
+++ gyms-by-fighter.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-gyms-by-fighter.php");
+
+$pageTitle = "gyms by fighter";
+include "view-header.php";
+$gyms = selectgymsbyfighter($_GET['id']);
+include "view-gyms-by-fighter.php";
+      include "view-footer.php";
+?>
