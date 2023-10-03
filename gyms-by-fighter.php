07f1de3726b18d3e87af783b25f8a1671b0c5197
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-03 18:26:55 -0500

    Update gyms-by-fighter.php

--- gyms-by-fighter.php
+++ gyms-by-fighter.php
@@ -4,7 +4,7 @@ require_once("model-gyms-by-fighter.php");
 
 $pageTitle = "gyms by fighter";
 include "view-header.php";
-$gym = selectgymsbyfighter($_GET['id']);
+$gyms = selectgymsbyfighter($_GET['id']);
 include "view-gyms-by-fighter.php";
       include "view-footer.php";
 ?>
