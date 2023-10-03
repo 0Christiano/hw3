0f8fb1ae524dbf8692cb5fb8d0ef2e894d57b13c
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-03 15:08:01 -0500

    Update fighter.php

--- fighter.php
+++ fighter.php
@@ -5,6 +5,6 @@ require_once("model-fighter.php");
 $pageTitle = "Fighter";
 include "view-header.php";
 $fighter = selectfighter();
-include "view-instructors.php";
+include "view-fighter.php";
       include "view-footer.php";
 ?>
