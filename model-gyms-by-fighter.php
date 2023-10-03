ab2ecc402da253567df263e58a235b62af401a23
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-03 16:58:01 -0500

    Create model-gyms-by-fighter.php

---
+++ model-gyms-by-fighter.php
@@ -0,0 +1,16 @@
+<?php
+function selectgymsbyfighter($fid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT g.gym_ID, gym_name, gym_description,style, origin  FROM `gym` g join MArt m on g.gym_ID = m.gym_id where m.fighter_id=?");
+      $stmt->bind_param("i", $fid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
