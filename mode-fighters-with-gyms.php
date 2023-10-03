6fffaf3d712006d4025458a6adedc40b1d6a3787
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-03 17:25:36 -0500

    Create mode-fighters-with-gyms.php

---
+++ mode-fighters-with-gyms.php
@@ -0,0 +1,15 @@
+<?php
+function selectfighters() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT fighter_ID,fighter_name,weight FROM `fighter` ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
