68169ea946b854af887e9330e27c38aa7a367a49
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-03 18:00:49 -0500

    Update fighters-with-gyms.php

--- fighters-with-gyms.php
+++ fighters-with-gyms.php
@@ -5,6 +5,6 @@ require_once("model-fighters-with-gyms.php");
 $pageTitle = "Fighters with Gyms";
 include "view-header.php";
 $fighters = selectfighters();
-include "view-fighter-with-gyms.php";
+include "view-fighters-with-gyms.php";
       include "view-footer.php";
 ?>
