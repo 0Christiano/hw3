fea560bb0cbe6b80bdfd4403ed3da8c7f60217c0
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-24 16:10:00 -0500

    Create Promotion.php

---
+++ Promotion.php
@@ -0,0 +1,37 @@
+<?php
+require_once("util-db.php");
+require_once("Promotion.php");
+
+$pageTitle = "promotion";
+include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+        case "Add" :
+         if (insertPromotion($_POST['pName'])) {
+           echo '<div class="alert alert-success" role="alert"> Promotion added</div>';
+         } else {
+           echo '<div class="alert alert-danger" role="alert"> Error</div>';
+         }
+        break;
+    case "Edit" :
+         if (updatePromotion($_POST['pName'], $_POST['pid'])) {
+           echo '<div class="alert alert-success" role="alert"> Promotion edited</div>';
+         } else {
+           echo '<div class="alert alert-danger" role="alert"> Error</div>';
+         }
+        break;
+    case "Delete" :
+         if (deletePromotion($_POST['pid'])) {
+           echo '<div class="alert alert-success" role="alert"> Promotion deleted</div>';
+         } else{
+           echo '<div class="alert alert-danger" role="alert"> Error</div>';
+         }
+        break;
+  }
+}
+
+$fighters = selectPromotion();
+include "view-promotion.php";
+      include "view-footer.php";
+?>
