2caf8e201e8857bddd70b0ab2640e32b69d42b20
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-23 19:13:10 -0500

    Update model-gym.php

--- model-gym.php
+++ model-gym.php
@@ -12,4 +12,44 @@ function selectgyms() {
         throw $e;
     }
 }
-?>
+
+function insertgym($gName, $gDesc) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `gym` (`gym_name`, `gym_description`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $gName, $gDesc);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updategym($gName, $gDesc, $gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `gym` set (`gym_name` = ?, `gym_description` = ? where gym_ID = ?");
+        $stmt->bind_param("ssi", $gName, $gDesc, $gid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deletegym($gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from gym where gym_ID = ?");
+        $stmt->bind_param("i", $gid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}?>
