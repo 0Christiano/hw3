30cc118a9bf6997d7105cbc81e526d40a52d39ec
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-24 16:03:09 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -19,6 +19,9 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
+         <li class="nav-item">
+          <a class="nav-link" href="fighter.php">fighter</a>
+        </li>
         <li class="nav-item">
           <a class="nav-link" href="fighter.php">fighter</a>
         </li>
