b89327325bc47d6777f614bcc2b0497b151cd57e
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-23 19:56:09 -0500

    Update gym.php

--- gym.php
+++ gym.php
@@ -4,6 +4,15 @@ require_once("model-gym.php");
 
 $pageTitle = "gym";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+        case "Add" :
+         insertGym($_POST['gNumber'], $_POST['gDesc']);
+        break;
+  }
+}
+
 $gyms = selectgyms();
 include "view-gym.php";
       include "view-footer.php";
