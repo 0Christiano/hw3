e1a39c0bf8cfb7b3c987efc27c334649970c5b3c
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-25 09:45:21 -0500

    Create model-brand.php

---
+++ model-brand.php
@@ -0,0 +1,55 @@
+<?php
+function selectBrand() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT brand_ID, brand_name FROM Brand");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertBrand($bName) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Brand` (`brand_name`) VALUES (?)");
+        $stmt->bind_param("s", $pName);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateBrand($bName, $bid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `brand` set `brand_name` = ?, where brand_ID = ?");
+        $stmt->bind_param("si", $bName, $bid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteBrand($bid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from Brand where brand_ID = ?");
+        $stmt->bind_param("i", $bid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}?>
