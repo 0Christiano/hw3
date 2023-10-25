47bccbfae2cadaf34f4425e6dce96cbcc77cac9a
0Christiano <142837529+0Christiano@users.noreply.github.com>
2023-10-25 09:23:55 -0500

    Update fighters-with-gyms.php

--- fighters-with-gyms.php
+++ fighters-with-gyms.php
@@ -4,37 +4,7 @@ require_once("model-fighters-with-gyms.php");
 
 $pageTitle = "Fighters with Gyms";
 include "view-header.php";
-
-
-
-if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
-        case "Add" :
-         if (insertMArt($_POST['mStyle'], $_POST['mOrigin'])) {
-           echo '<div class="alert alert-success" role="alert"> MArt added</div>';
-         } else {
-           echo '<div class="alert alert-danger" role="alert"> Error</div>';
-         }
-        break;
-    case "Edit" :
-         if (updateMArt($_POST['mStyle'], $_POST['mOrigin'], $_POST['mid'])) {
-           echo '<div class="alert alert-success" role="alert"> MArt edited</div>';
-         } else {
-           echo '<div class="alert alert-danger" role="alert"> Error</div>';
-         }
-        break;
-    case "Delete" :
-         if (deleteMArt($_POST['mid'])) {
-           echo '<div class="alert alert-success" role="alert"> MArt deleted</div>';
-         } else{
-           echo '<div class="alert alert-danger" role="alert"> Error</div>';
-         }
-        break;
-  }
-}
-
-
-$MArts = selectMArts();
+$fighters = selectfighters();
 include "view-fighters-with-gyms.php";
       include "view-footer.php";
 ?>
